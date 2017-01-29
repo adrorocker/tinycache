48514581788492882396a260690dc9f0c7a2b60f
Alejandro Morelos <adro.skate@gmail.com>
2017-01-28 21:53:09 -0600

    Carpe Diem

---
+++ src/Cache.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * TinyCache
+ *
+ * @link      https://github.com/adrorocker/tinycache
+ * @copyright Copyright (c) 2017 Adro Rocker
+ * @author    Adro Rocker <alejandro.morelos@jarwebdev.com>
+ */
+namespace TinyCache;
+
+class Cache
+{
+
+}
